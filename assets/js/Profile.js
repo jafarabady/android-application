e82ae52b15a4018f006e979f7e4244135670da8d
mahdi <109016246+mahdi81007@users.noreply.github.com>
2023-11-01 21:33:44 +0330

    nothing

---
+++ assets/js/Profile.js
@@ -0,0 +1,6 @@
+function toEnglishDigits(str) {
+    const persianNumbers = ["۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹", "۰"]
+    const englishNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
+    return str.split("").map(c => englishNumbers[persianNumbers.indexOf(c)]|| c).join("")
+}
