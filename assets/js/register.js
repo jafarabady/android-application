a6d0e49a13c695d2e4c61d8c93d6f0f2eb5fef44
mahdi <109016246+mahdi81007@users.noreply.github.com>
2023-11-03 02:54:14 +0330

    edit register

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,14 +1,20 @@
 const formRegister = document.querySelector('#register-form');
 const urlRegister = 'https://ghafmesle.iran.liara.run/api/client/register/';
+const password = document.querySelector("#password")
+const passwordRepeat = document.querySelector("#password-repeat")
+console.log(passwordRepeat)
 formRegister.addEventListener('submit', (event) => {
     event.preventDefault();
 
     const formData = new FormData(formRegister);
-
+const password1 =password.value
+    const password2 = passwordRepeat.value
+    console.log(password1,password2)
     fetch(urlRegister, {
         method: 'POST',
         body: formData,
     })
+
         .then(response => response.json())
         .then(data => {
             // پاسخ موفقیت‌آمیز
@@ -19,7 +25,16 @@ formRegister.addEventListener('submit', (event) => {
                 icon: 'success',
                 confirmButtonText: 'حله'
             })
-
+            setTimeout(() => {
+                window.location.href="login.html"
+            },3000)
+        })
+    if (password1 !==password2){
+        Swal.fire({
+            title: 'رمز عبور ها یکسان نیستند ',
+            text: 'دوباره امتحان کن',
+            icon: 'error',
+            confirmButtonText: 'حله!'
         })
         .catch(error => {
             // پاسخ خطا
@@ -32,4 +47,6 @@ formRegister.addEventListener('submit', (event) => {
             })
 
         });
+
+    }
 });
